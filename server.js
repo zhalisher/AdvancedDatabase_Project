ccad592107794ae0647a167560a1d00dd88b6bea
ballm <alisherzhanibek@mail.ru>
2025-12-26 18:37:52 +0500

    adding additional routes

--- server.js
+++ server.js
@@ -18,7 +18,18 @@ app.get('/signup.html', (req, res) => {
 app.get('/login.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
-
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+app.get('/landing.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'landing.html'));
+});
+app.get('/main.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'main.html'));
+});
+app.get('/watch.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'watch.html'));
+});
 
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
