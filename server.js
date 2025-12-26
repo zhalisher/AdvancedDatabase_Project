6fda3b81c250af457f3ba0304806cf31478ff4c4
ballm <alisherzhanibek@mail.ru>
2025-12-26 16:34:09 +0500

    .

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'landing.html'));
+});
+
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
